cf3fe649ae828ffc41dc4889031b874b959e4e5e
Elena Nikitina <shorstko@gmail.com>
2017-01-22 22:29:43 +0300

    Add files via upload

--- Parser.py
+++ Parser.py
@@ -3,18 +3,14 @@
 
 import urllib.request
 from bs4 import BeautifulSoup
-import pandas as pd
-import html
 from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 from collections import Counter
 from pandas import DataFrame
 import numpy as np
-import operator
 import csv
-import random
-import time
 
+#фунцкция-парсер даты с Хабра
 months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря']
 def StrToDate(strdate):
     parts = list(filter(None, (strdate).split(' ')))
@@ -24,7 +20,6 @@ def StrToDate(strdate):
         elif parts[0] == 'вчера':
             publishdate = (datetime.now() - timedelta(days=1)).date()
         else:
-            formatter_string = "%d-%m-%y"
             datestring = parts[0] + '-' + str(months.index(parts[1]) + 1)
             if parts[2] == 'в':
                 datestring = datestring + '-' + str(datetime.today().year)
@@ -33,6 +28,7 @@ def StrToDate(strdate):
             publishdate = datetime.strptime(datestring, '%d-%m-%Y').date()
     return publishdate
 
+#функция, преобразующая текстовое представление числа (1,4k) в число
 def ViewsToNumber(strvcount):
     coeff = 1
     coeff2 = 1
@@ -66,14 +62,10 @@ try: response = urllib.request.urlopen(url_full)
 except urllib.error.URLError as e:
     print('Ошибка при открытии страницы: ', e.reason)
 
-
-
-url_real = response.geturl()
-htmlinfo = response.info()
+url_real = response.geturl() # вот так можно проверить "правильную" ссылку - в случае, если есть перенаправление
 html = response.read()
 
-#собираем все страницы - результаты поиска и складываем их url'ы в переменную urls
-#учитываем, что выводятся не все результаты поиска, поэтому сначала нужно выяснить номер последней страницы
+#учитываем, что результаты поиска выводятся постранично, поэтому сначала нужно выяснить номер последней страницы
 soup = BeautifulSoup(html)
 html_lastpage = soup.find('ul', attrs={'id':'nav-pages'}).find('a', attrs={'title':'Последняя страница'}).get('href')
 start = html_lastpage.find('page') + len('page')
@@ -81,14 +73,13 @@ end = len(html_lastpage)
 lastpage = int(html_lastpage[start:end-1])
 
 
-#первый фильтр: берем все статьи с лейблом tutorial (Обучающий материал)
+#берем все статьи с лейблом tutorial (Обучающий материал)
 #и для них сразу получаем максимум информации, которая сейчас доступна: дата публикации, название, ссылка, хабы и пр.
 page = 0
 tutorials = []
-#tutorial: { date, title, url, {hubs}, views, favoritecount, commentscount, author }
-random.seed()
-while page < lastpage:
-#while page < 12:
+#tutorial: { date, title, url, {hubs}, views, favoritecount, commentscount, author, {tags} }
+#while page < lastpage:
+while page < 1:
     page = page + 1
     url = 'https://habrahabr.ru/hub/python/page' + str(page) + '/'
     matches = BeautifulSoup(urllib.request.urlopen(url).read()).find_all('span', attrs={'class': 'flag_tutorial', 'title': 'Обучающий материал'})
@@ -135,8 +126,7 @@ while page < lastpage:
             for tag in tagsarr:
                 tags.append(tag.text)
         except:
-            time.sleep(1)
-            #tagsarr = BeautifulSoup(urllib.request.urlopen(articleurl).read()).find_all('a', attrs={'rel': 'tag'})
+            pass
         tutorial.append(tags)
         #сохраняем очередную запись о статье в массив статей
         tutorials.append(tutorial)
@@ -150,49 +140,72 @@ with open("output.csv", "w", encoding='cp1251', newline='') as f:
     writer = csv.writer(f, 'customcsv')
     writer.writerows(tutorials)
 
+#загружаем всю информацию о 222 статьях из CSV-файла
+tutorials = []
+with open('все туториалы по питону с 2009 года.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=';')
+    for row in readCSV:
+        # tutorial: { date, title, url, {hubs}, views, favoritecount, commentscount, author, {tags} }
+        tutorial = []
+        tutorial.append(datetime.strptime(row[0], '%Y-%m-%d').date())
+        tutorial.append(row[1])
+        tutorial.append(row[2])
+        hubs = list(str(row[3]).replace('[', '').replace(']', '').replace('\'', '').split(','))
+        tutorial.append(hubs)
+        tutorial.append(row[4])
+        tutorial.append(row[5])
+        tutorial.append(row[6])
+        tutorial.append(row[7])
+        tags = list(str(row[8]).replace('[', '').replace(']', '').replace('\'', '').split(','))
+        tutorial.append(tags)
+
+        tutorials.append(tutorial)
+
+
 #считаем статистику: распределение обучающих материалов по годам, топ-20 статей по: добавлению в избранное,
 #самые обсуждаемые (по количеству комментариев), самый пишущий автор
 dates = []
 mostfavored = []
 mostcommented = []
 authors = []
-id = 0
+
 for tutorial in tutorials:
     dates.append(tutorial[0].year) #распределение по годам
-    mostfavored.append([id, tutorial[5]])
-    mostcommented.append([id, tutorial[6]])
+    mostfavored.append([tutorial[1], tutorial[5]])
+    mostcommented.append([tutorial[1], tutorial[6]])
     authors.append(tutorial[7])
-    id+= 1
 
+# пример двух вариантов сортировки: либо объявляем и передаем функцию, либо используем lambda-выражение
 def customsort(i):
     return i[1]
 
-stats = Counter(dates).items()
+dictstats = Counter(dates).items()
+stats = []
+for ds in dictstats:
+    stats.append(ds)
+stats.sort()
 mostfavored.sort(key=lambda i: i[1], reverse= True)
 mostcommented.sort(key=customsort, reverse = True)
 statsauthors = Counter(authors).most_common()
 
-#[(l,k) for k,l in sorted([(j,i) for i,j in statsauthors.items()], reverse=True)]
-
-
-#строим визуализации: распределение статей по годам, рейтинг самых пишущих авторов
-#plt.interactive(False)
-plt.rc('font', family='Arial')
+#строим визуализацию по распределению статей по годам
+plt.rc('font', family='Arial') #задаем шрифт явно, иначе кириллица не отображается
 
 figure = plt.figure(1)
-#pltcounts = plt.subplot(121)
 plt.title('Распределение статей по годам')
+
 x = []
 y = []
 for stat in stats:
-    x.append(stat[0])
+    x.append(int(stat[0]))
     y.append(stat[1])
+plt.xkcd()
 plt.plot(x, y)
 plt.interactive(False)
+plt.xticks(x, x)
 plt.show(block=True)
 
-#pltauthors = plt.subplot(122)
-
+#строим визуализацию по самым пишущим авторам
 figure = plt.figure(2)
 plt.title('Самые пишущие авторы')
 x = []
@@ -210,20 +223,61 @@ pos = np.arange(len(y))
 plt.title('Топ-20 самых пишущих авторов')
 plt.barh(pos, y)
 
-#add the numbers to the side of each bar
+#записываем количество статей справа от полосы
 for p, c, ch in zip(pos, x, y):
     plt.annotate(str(ch), xy=(ch + 1, p + .5), va='center')
 
-#cutomize ticks
+#настраиваем область диаграммы
 ticks = plt.yticks(pos + .5, x)
 xt = plt.xticks()[0]
 plt.xticks(xt, [' '] * len(xt))
 plt.grid(axis = 'x', color ='white', linestyle='-')
-
-#set plot limits
 plt.ylim(pos.max(), pos.min() - 1)
 plt.xlim(0, 30)
-#plt.interactive(False)
 plt.show(block=True)
+
+#выводим топ-20 статей по добавлению в избранное
+id = 0
+print('\n***Топ-20 статей по добавлению в избранное***')
+for fav in mostfavored:
+    id+= 1
+    print(str(id) + '. ' + fav[0] + ' (' + str(fav[1]) + ')')
+    if id == 20:
+        break
+
+#выводим топ-20 статей по добавлению в избранное
+id = 0
+print('\n***Топ-20 комментируемых статей***')
+for comm in mostcommented:
+    id+= 1
+    print(str(id) + '. ' + comm[0] + ' (' + str(comm[1]) + ')')
+    if id == 20:
+        break
+
+#насколько совпадают самые добавляемые и самые комментируемые?
+id = 0
+favs = set()
+for fav in mostfavored:
+    id+= 1
+    favs.add(fav[0])
+    if id == 20:
+        break
+
+id = 0
+comms = set()
+for com in mostcommented:
+    id+= 1
+    comms.add(com[0])
+    if id == 20:
+        break
+
+result = set(favs) & set(comms)
+
+print('\n***Самые популрные статьи***')
+id = 0
+for res in result:
+    id+= 1
+    print (str(id) + '. ' + res)
+    id += 1
 
 print("Мы сделали это!")
